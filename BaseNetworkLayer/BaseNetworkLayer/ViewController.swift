e1e44c0959de803ab21211ae29c977539dcdb762
Gregory <grishavinyar@icloud.com>
2022-09-06 21:10:11 +0300

    [init] the project

---
+++ BaseNetworkLayer/BaseNetworkLayer/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BaseNetworkLayer
+//
+//  Created by Григорий Виняр on 06/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .white
+    title = "Posts"
+  }
+
+
+}
+
